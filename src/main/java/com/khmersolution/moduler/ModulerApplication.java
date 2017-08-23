d559e193f5b2a1553947d39bb1c3d8abcbc8f656
Ravuthz <ravuthz@gmail.com>
2017-08-23 14:34:33 +0700

    Deploy to Heroku Hosting

--- src/main/java/com/khmersolution/moduler/ModulerApplication.java
+++ src/main/java/com/khmersolution/moduler/ModulerApplication.java
@@ -2,8 +2,14 @@ package com.khmersolution.moduler;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.khmersolution.moduler")
+@EntityScan(basePackages = "com.khmersolution.moduler.domain")
+@EnableJpaRepositories(basePackages = "com.khmersolution.moduler.repository")
 public class ModulerApplication {
 
 	public static void main(String[] args) {
