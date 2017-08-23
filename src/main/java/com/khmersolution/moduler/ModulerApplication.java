e3f605d48a6edf1818ac2460ea8ec5939355e0f7
Ravuthz <ravuthz@gmail.com>
2017-08-23 09:57:56 +0700

    Init Gradle Project

---
+++ src/main/java/com/khmersolution/moduler/ModulerApplication.java
@@ -0,0 +1,12 @@
+package com.khmersolution.moduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModulerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModulerApplication.class, args);
+	}
+}
---
+++ src/test/java/com/khmersolution/moduler/ModulerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.khmersolution.moduler;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ModulerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
